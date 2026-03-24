2b570f1c0182f3835713b532e996142bbf2c1109
Dikeledi Mokoatle <2769106@students.wits.ac.za>
2026-03-24 18:48:23 +0200

    Created a User schema with name, email, password, verified status and created_at

---
+++ backend/common/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+
+new userSchema = ({
+    name:{type: String, required: true, trim:true},
+    email:{type: String, required:true, unique:true, lowercase: true, trim:true},
+    password:{type:String, required:true},
+    verified:{type: Boolean, default: false},
+    created_at:{type: Date, default: Date.now},
+});
+const User = model('User', userSchema);
+module.exports = User;
