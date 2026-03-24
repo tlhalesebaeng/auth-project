0ed6dfbea5b91f95c7b6d35f89e2625c246174de
Dikeledi Mokoatle <2769106@students.wits.ac.za>
2026-03-25 01:46:11 +0200

    implement front end js, event listeners, error message and success message visibility

--- register/register.js
+++ register/register.js
@@ -1,8 +1,55 @@
+const errorMessage = document.getElementById('error-message');
+const form = document.getElementById('registration-form');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const confirmPassword = document.getElementById('confirm-password');
+const password = document.getElementById('new-password');
 import { navigate } from '../main/index.js';
 
-const registerButton = document.getElementById('register-btn');
+errorMessage.style.display = "none";
 
-registerButton.addEventListener('click', (event) => {
-    event.preventDefault(); 
-    navigate('../verify-email/verify-email.html');
-});
+
+form.addEventListener('submit', async (event)=>{
+    event.preventDefault();
+
+    errorMessage.style.display = "none";
+    confirmPassword.classList.remove("input-error");
+
+    if (password.value != confirmPassword.value){
+        confirmPassword.classList.add("input-error");
+        errorMessage.style.display = "block";
+        errorMessage.textContent = "Passwords Do Not Match!";
+        return;
+    }
+
+    try{
+        const response = await fetch('http://localhost:3000/do-register',{
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({
+                firstName : firstName.value,
+                lastName: lastName.value,
+                email : email.value,
+                password : password.value,
+                confirmPassword: confirmPassword.value
+            })
+        });
+
+        const data = await response.json();
+        if (data.success){
+            alert(`${firstName.value} registered successfully` );
+            navigate('../login/login.html');
+        }
+        else{
+            errorMessage.style.display = "block";
+            errorMessage.textContent = data.error;
+        }
+
+    }catch(err){
+        errorMessage.style.display = "block";
+        errorMessage.textContent = err.message;
+    }
+})
